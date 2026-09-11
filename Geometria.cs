3859b842f87034bbb4337b69bf1c511ae110065e
lauraespadin <laura.espadin@edu.senai.br>
2026-09-11 12:13:48 -0300

    Adicionar arquivos de projeto.

---
+++ Geometria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Heranca
+{
+    public class Geometria
+    {
+        
+        public double CalcularArea(double lado)
+        {
+            return lado * lado;
+        }
+
+        public double CalcularArea(double baseRetangulo, double altura)
+        {
+            return baseRetangulo * altura;
+        }
+
+        public double CalcularArea(double raio, bool isCirculo)
+        {
+            return Math.PI * raio * raio;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Geometria geometria = new Geometria();
+
+            double areaQuadrado = geometria.CalcularArea(5);
+
+            double areaRetangulo = geometria.CalcularArea(40, 4);
+
+            double areaCirculo = geometria.CalcularArea(6, true);
+
+            Console.WriteLine($"Área do quadrado: {areaQuadrado:F2}");
+            Console.WriteLine($"Área do retângulo: {areaRetangulo:F2}");
+            Console.WriteLine($"Área do círculo: {areaCirculo:F2}");
+        }
+    }
+}
